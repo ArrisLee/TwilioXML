9b4df2b6bd753e99bcd9e8e64df25b81d70c05a7
ArrisLee <arris_li@hotmail.com>
2018-09-18 23:11:11 +1200

    ###updated

--- controller/controller.go
+++ controller/controller.go
@@ -8,7 +8,7 @@ import (
 )
 
 //GetXML func
-//pass text and retrieve XML file - TwiML to make the call
+//pass text and retrieve XML to make the call
 func GetXML(c echo.Context) error {
 	type TwiML struct {
 		XMLName xml.Name `xml:"Response"`
@@ -16,7 +16,7 @@ func GetXML(c echo.Context) error {
 	}
 	text := c.QueryParam("text")
 	if text == "" {
-		text = "Hello, this is the call from, GOGO, you have a new order!"
+		text = "Hello, this is the call from: GOGO, you have a new order!"
 	}
 	twiml := TwiML{Say: text}
 	return c.XML(http.StatusOK, twiml)
